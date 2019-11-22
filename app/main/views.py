65bac9d63c34e7b2add100c890e9821c23893640
dan <dan@localhost.localdomain>
2019-11-22 14:40:06 +0300

    create the required folders for the aplication

--- app/main/views.py
+++ app/main/views.py
@@ -1,8 +1,10 @@
-from flask import render_template,url_for,request,redirect
+from flask import render_template,url_for,request,redirect,abort
 from . import main
 from ..request import get_movies,get_movie,search_movie
-from ..models import Review
-from .forms import ReviewForm
+from ..models import Review,User
+from .forms import ReviewForm,UpdateProfile
+from flask_login import login_required, current_user 
+from .. import db,photos
 
 
 
@@ -37,8 +39,8 @@ def movie(id):
     '''
     movie = get_movie(id)
     title = f'{movie.title}'
-    reviews = Review.get_reviews(movie.id)
-    return render_template('movie.html',title = title, movie= movie, reviews = reviews )
+    reviewx = Review.get_reviews(movie.id)
+    return render_template('movie.html',title = title, movie= movie, reviewx = reviewx )
     
 
 @main.route('/search/<movie_name>')
@@ -53,6 +55,7 @@ def search(movie_name):
     return render_template('search.html',movies = searched_movies)
     
 @main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+@login_required
 def new_review(id):
     form = ReviewForm()
     movie = get_movie(id)
@@ -60,10 +63,52 @@ def new_review(id):
     if form.validate_on_submit():
         title = form.title.data
         review = form.review.data
-        new_review = Review(movie.id,title,movie.poster,review)
 
-        return redirect(url_for('movie',id= movie.id))
+        new_review = Review(movie_id=movie.id,movie_title= title,image_path = movie.poster,movie_review = review,user = current_user)
+
+        # save review method
+        new_review.save_review()
+        return redirect(url_for('.movie',id = movie.id))
+
+        
     title = f'{movie.title} review'
     return render_template('new_review.html', title= title, review_form = form,movie=movie)
 
-    
+@main.route('/user/<uname>')
+def profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+
+    return render_template("profile/profile.html", user = user)
+
+@main.route('/user/<uname>/update',methods = ['GET','POST'])
+@login_required
+def update_profile(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if user is None:
+        abort(404)
+    form = UpdateProfile()
+
+    if form.validate_on_submit():
+        user.bio = form.bio.data 
+
+        db.session.add(user)
+        db.session.commit()
+
+        return redirect(url_for('.profile',uname = user.username))  
+        return render_template('profile/update.html', form = form) 
+
+@main.route('/user/<uname>/update/pic', methods = ['POST'])
+@login_required
+def update_pic(uname):
+    user = User.query.filter_by(username = uname).first()
+
+    if 'photo' in request.files:
+        filename = photos.save(request.files['photo'])
+        path = f'photos/{filename}'
+        user.profile_pic_path = path
+        db.session.commit()
+    return redirect(url_for('main.profile',uname = uname))
